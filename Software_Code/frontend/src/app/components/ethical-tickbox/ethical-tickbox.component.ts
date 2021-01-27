3500a7f8e2869d8b4b9114ce6a918951332bc949
skyekirwan <scoobyskye@aol.com>
2021-01-27 15:31:11 +0000

    add tickbox component with bootstrap

---
+++ Software_Code/frontend/src/app/components/ethical-tickbox/ethical-tickbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ethical-tickbox',
+  templateUrl: './ethical-tickbox.component.html',
+  styleUrls: ['./ethical-tickbox.component.css']
+})
+export class EthicalTickboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
