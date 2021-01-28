3f8f06a63ee9676e402ea95477a1b4d29df8fc66
skyekirwan <scoobyskye@aol.com>
2021-01-28 11:40:29 +0000

    added university styling

--- Software_Code/frontend/src/app/components/experiment/experiment.component.ts
+++ Software_Code/frontend/src/app/components/experiment/experiment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ExperimentDetails} from "../../model/request/experiment-details";
 
 @Component({
   selector: 'app-experiment',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiment.component.css']
 })
 export class ExperimentComponent implements OnInit {
+  experiment: ExperimentDetails;
 
-  constructor() { }
+  constructor()
+   {
+     this.experiment = new ExperimentDetails();
+   }
 
   ngOnInit(): void {
+    this.experiment.title = "Experiment Title";
+    this.experiment.description = "For digital applications always use the specified RGB breakdowns for each colours. The colour breakdowns in this guide have been formulated for optimum results on coated matt sheets. If printing in CMYK, always use the exact colour breakdowns specified.";
+    this.experiment.type = "Questionnaire";
+
   }
 
 }
