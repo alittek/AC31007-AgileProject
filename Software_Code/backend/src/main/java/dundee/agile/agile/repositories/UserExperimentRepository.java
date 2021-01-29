8e63f28868d83a06081404f121c4f29d802bd0fb
jokbutkus <jok.butkus@gmail.com>
2021-01-29 12:15:03 +0200

    Lab manager access to all experiments

--- Software_Code/backend/src/main/java/dundee/agile/agile/repositories/UserExperimentRepository.java
+++ Software_Code/backend/src/main/java/dundee/agile/agile/repositories/UserExperimentRepository.java
@@ -1,14 +1,18 @@
 package dundee.agile.agile.repositories;
 
 import dundee.agile.agile.model.database.Experiment;
+import dundee.agile.agile.model.database.User;
 import dundee.agile.agile.model.database.UserExperiment;
 import dundee.agile.agile.model.enums.Privileges;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserExperimentRepository extends JpaRepository<UserExperiment, Long> {
     Optional<UserExperiment> findByExperimentAndLevelOfPrivileges(Experiment experiment, Privileges levelOfPrivileges);
+
+    List<UserExperiment> findAllByUser(User user);
 }
