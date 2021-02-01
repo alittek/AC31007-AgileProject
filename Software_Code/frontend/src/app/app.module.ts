4c5cff4a6ff649201e70be4f52611411b506ec8a
budryte <43143936+budryte@users.noreply.github.com>
2021-02-01 19:10:14 +0200

    new questionnaire component created

--- Software_Code/frontend/src/app/app.module.ts
+++ Software_Code/frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {ExperimentComponent} from './components/experiment/experiment.component'
 import {EthicalTickboxComponent} from './components/ethical-tickbox/ethical-tickbox.component';
 import {CreateQuestionnaireComponent} from './components/create-questionnaire/create-questionnaire.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { QuestionnaireComponent } from './components/questionnaire/questionnaire.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
     ExperimentComponent,
     EthicalTickboxComponent,
     CreateQuestionnaireComponent,
+    QuestionnaireComponent,
   ],
   imports: [
     BrowserModule,
