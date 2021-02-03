adcb8f9fe1c22e18fed108b7056d23bd270e3657
Calum <calumalogan@gmail.com>
2021-02-03 08:21:49 +0000

    added typescript beginnings

--- Software_Code/frontend/src/app/components/question-multi/question-multi.component.ts
+++ Software_Code/frontend/src/app/components/question-multi/question-multi.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpService} from '../../services/http.service';
+import { QuestionDetails} from "../../model/request/question-details";
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-question-multi',
   templateUrl: './question-multi.component.html',
   styleUrls: ['./question-multi.component.css']
 })
-export class QuestionMultiComponent implements OnInit {
+export class QuestionMultiComponent {
+  data: QuestionDetails;
 
-  constructor() { }
+  constructor() {
+    // pass data from backend in here?
+  }
 
-  ngOnInit(): void {
+  addOptions(){
+    // set options to backend here?
   }
 
 }
