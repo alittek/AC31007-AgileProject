b93a21daf1419a541a1dffa38f6bda25dd321260
budryte <43143936+budryte@users.noreply.github.com>
2021-02-04 13:31:58 +0200

    displaying questionnaire details

--- Software_Code/frontend/src/app/components/questionnaire/questionnaire.component.ts
+++ Software_Code/frontend/src/app/components/questionnaire/questionnaire.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QuestionDetails} from '../../model/request/question-details';
 import {ActivatedRoute} from '@angular/router';
+import {HttpService} from '../../services/http.service';
+import {QuestionnaireDetails} from '../../model/request/questionnaire-details';
 
 @Component({
   selector: 'app-questionnaire',
@@ -10,17 +12,26 @@ import {ActivatedRoute} from '@angular/router';
 export class QuestionnaireComponent implements OnInit {
   questionnaireRouteId: number;
   questions: QuestionDetails[];
+  questionnaire: QuestionnaireDetails;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private httpService: HttpService) {
     this.questions = new Array<QuestionDetails>();
   }
 
   ngOnInit(): void {
     this.getQuestionnaireRouteId();
+    this.getQuestionnaire();
   }
 
   getQuestionnaireRouteId(): void {
     this.route.params.subscribe(params => this.questionnaireRouteId = params.id);
   }
 
+  getQuestionnaire(): void {
+    this.httpService.getQuestionnaire(this.questionnaireRouteId).subscribe(value => {
+      this.questionnaire = value;
+      console.log(value);
+    });
+  }
 }
