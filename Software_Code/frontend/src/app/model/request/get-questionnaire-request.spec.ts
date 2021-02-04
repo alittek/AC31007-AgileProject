06304fe3cd8e7091ea3d78e6a7fa42dcf7a9687f
budryte <43143936+budryte@users.noreply.github.com>
2021-02-04 13:29:52 +0200

    get questionnaire http method

---
+++ Software_Code/frontend/src/app/model/request/get-questionnaire-request.spec.ts
@@ -0,0 +1,7 @@
+import { GetQuestionnaireRequest } from './get-questionnaire-request';
+
+describe('GetQuestionnaireRequest', () => {
+  it('should create an instance', () => {
+    expect(new GetQuestionnaireRequest()).toBeTruthy();
+  });
+});
